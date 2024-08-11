0a49e882f39c96a12cc1fb4ceb5b238fc1e3bccb
rajdeepf1 <rajdeepf1@gmail.com>
2024-08-10 23:43:21 -0400

    learnt @Binds functionality

--- app/src/main/java/com/example/daggerexample/UserRepositoryModule.kt
+++ app/src/main/java/com/example/daggerexample/UserRepositoryModule.kt
@@ -1,20 +1,28 @@
 package com.example.daggerexample
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
-@Module
-class UserRepositoryModule {
+//@Module
+//class UserRepositoryModule {
+////    @Provides
+////    fun getUserFireBaseRepository() : UserRepositoryService{
+////        return FirebaseRepository()
+////    }
+//
+//    // or
+//
 //    @Provides
-//    fun getUserFireBaseRepository() : UserRepositoryService{
-//        return FirebaseRepository()
+//    fun getSQLRepository(sqlRepository: SQLRepository) : UserRepositoryService{
+//        return sqlRepository
 //    }
+//
+//}
 
-    // or
-
-    @Provides
-    fun getSQLRepository(sqlRepository: SQLRepository) : UserRepositoryService{
-        return sqlRepository
-    }
-
+// or
+@Module
+abstract class UserRepositoryModule {
+    @Binds
+    abstract fun getSQLRepository(sqlRepository: SQLRepository) : UserRepositoryService
 }
